61571bef834a70eda5bec3bfa82e2b48044fc9ce
manuelmezafray <72808591+manuelmezafray@users.noreply.github.com>
2022-03-02 22:10:20 -0600

    Prototipo Galeria
    Se crearon los archivos css y js necesarios para ver como queremos uno de los posibles resultados de nuestra galería

---
+++ js/gallery.js
@@ -0,0 +1,15 @@
+let galleryImages =document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth
+
+if(galleryImages) {
+    galleryImages.forEach(function(image) {
+        image.onclick = function() {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("/img/thumbnails/");
+            let setNewImgUrl = getImgUrlPos[1].replace('")', ' ');
+            alert(setNewImgUrl[1]);
+        }
+    });
+}
