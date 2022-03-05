599974c4b19b9828a1ef36be6b3b3198f35e01be
manuelmezafray <72808591+manuelmezafray@users.noreply.github.com>
2022-03-05 12:28:10 -0600

    Prototipo Galería 2.0

--- js/gallery.js
+++ js/gallery.js
@@ -3,13 +3,27 @@ let getLatestOpenedImg;
 let windowWidth = window.innerWidth
 
 if(galleryImages) {
-    galleryImages.forEach(function(image) {
+    galleryImages.forEach(function(image, index) {
         image.onclick = function() {
             let getElementCss = window.getComputedStyle(image);
             let getFullImgUrl = getElementCss.getPropertyValue("background-image");
             let getImgUrlPos = getFullImgUrl.split("/img/thumbnails/");
             let setNewImgUrl = getImgUrlPos[1].replace('")', ' ');
-            alert(setNewImgUrl[1]);
-        }
+
+           /* alert(setNewImgUrl)*/
+
+            getLatestOpenedImg = index + 1
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window")
+            newImgWindow.setAttribute("onclick", "closeImg()")
+
+            let newImg = document.createElement("img")
+            newImgWindow.appendChild(newImg)
+            newImg.setAttribute("src", "img/" + setNewImgUrl)
+
+            }
     });
 }
